aa229b0027667ade23661155f7c9d8ffd6ce9b60
marcel <marcel.taubert@epitech.eu>
2023-12-13 14:36:06 +0000

    add vm

---
+++ src/token.h
@@ -0,0 +1,112 @@
+#pragma once
+#include <string_view>
+#include <spdlog/spdlog.h>
+
+enum class TokenType : unsigned int {
+    LeftParen,
+    RightParen,
+    Dot,
+    Semicolon,
+
+    // math
+    Slash,
+    Star,
+    Minus,
+    Plus,
+
+    INumber,
+    DNumber,
+
+    Print,
+
+    Eof,
+};
+
+using namespace std::string_view_literals;
+
+constexpr std::array tokenTypeName = {
+    "LeftParen"sv,
+    "RightParen"sv,
+    "Dot"sv,
+    "Semicolon"sv,
+
+    "Slash"sv,
+    "Star"sv,
+    "Minus"sv,
+    "Plus"sv,
+
+    "INumber"sv,
+    "DNumber"sv,
+
+    "print"sv,
+
+    "EOF"sv,
+};
+
+[[nodiscard]] static constexpr auto getTokenTypeName(const TokenType ttype) -> std::string_view {
+    return tokenTypeName.at(static_cast<unsigned int>(ttype));
+}
+
+template<>
+struct fmt::formatter<TokenType> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx);
+
+    template<typename FormatContext>
+    auto format(const TokenType& type, FormatContext& ctx);
+};
+
+template<typename ParseContext>
+constexpr auto fmt::formatter<TokenType>::parse(ParseContext& ctx) {
+    return ctx.begin();
+}
+
+template<typename FormatContext>
+auto fmt::formatter<TokenType>::format(const TokenType& type, FormatContext& ctx) {
+    return fmt::format_to(ctx.out(), "{}", ""/*getTokenTypeName(type)*/);
+}
+
+
+struct TokenPosition {
+    unsigned int line;
+    unsigned int column;
+
+    [[nodiscard]] constexpr auto operator==(const TokenPosition& other) const noexcept -> bool {
+        return line == other.line && column == other.column;
+    }
+};
+
+struct Token {
+    TokenType ttype;
+    std::string_view lexeme;
+    TokenPosition position { 0, 0 };
+
+    [[nodiscard]] constexpr auto getLexeme() const -> std::string_view { 
+        return lexeme; 
+    }
+
+    [[nodiscard]] constexpr auto operator==(const Token& other) const -> bool  {
+        return this->ttype == other.ttype 
+            && this->lexeme == other.lexeme 
+            && this->position == other.position;
+    }
+};
+
+template<>
+struct fmt::formatter<Token> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx);
+
+    template<typename FormatContext>
+    auto format(const Token& token, FormatContext& ctx) const;
+};
+
+template<typename ParseContext>
+constexpr auto fmt::formatter<Token>::parse(ParseContext& ctx) {
+    return ctx.begin();
+}
+
+template<typename FormatContext>
+auto fmt::formatter<Token>::format(const Token& token, FormatContext& ctx) const {
+    return fmt::format_to(ctx.out(), "{} '{}' at [{},{}]", token.ttype, token.lexeme, token.position.line, token.position.column);
+}
---
+++ src/vm.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "gen.h"
+#include <iostream>
+#include <iterator>
+#include <variant>
+
+using INumber = int;
+using DNumber = double;
+using Bool = bool;
+using Value = std::variant<Bool, INumber, DNumber>;
+
+struct PrintVisitor {
+    std::string operator()(Bool b) { return b ? "true" : "false"; }
+    // This matches every other type than std::monostate. 
+    std::string operator()(const auto& x) { return fmt::format("{}", x); }
+};
+
+class VirtualMachine {
+    enum class BinaryOperators {
+        ADD,
+        SUB,
+    };
+public:
+    VirtualMachine(std::span<ByteCode::Type> bytecode) : bytecode { bytecode }{ ip = std::begin(bytecode); }
+
+    template<typename T>
+    auto readConstant(void) -> T {
+        T value;
+        assert(std::distance(ip, std::end(this->bytecode)) >= sizeof(T));
+        std::memcpy(&value, &(*ip), sizeof(T));
+        std::advance(ip, sizeof(T));
+        return value;
+    }
+
+    auto doBinaryOperation(BinaryOperators op) -> void {
+        auto b = stack.back();
+        stack.pop_back();
+
+        auto a = stack.back();
+        stack.pop_back();
+
+        switch (op) {
+        case BinaryOperators::ADD:
+            if (std::holds_alternative<INumber>(a) && std::holds_alternative<INumber>(b)) {
+                stack.emplace_back(std::get<INumber>(a) + std::get<INumber>(b));
+            } else if (std::holds_alternative<DNumber>(a) && std::holds_alternative<DNumber>(b)) {
+                stack.emplace_back(std::get<DNumber>(a) + std::get<DNumber>(b));
+            } else {
+                assert(false && "type mismatch");
+            }
+            break;
+
+        case BinaryOperators::SUB:
+            if (std::holds_alternative<INumber>(a) && std::holds_alternative<INumber>(b)) {
+                stack.emplace_back(std::get<INumber>(a) - std::get<INumber>(b));
+            } else if (std::holds_alternative<DNumber>(a) && std::holds_alternative<DNumber>(b)) {
+                stack.emplace_back(std::get<DNumber>(a) - std::get<DNumber>(b));
+            } else {
+                assert(false && "type mismatch");
+            }
+            break;
+        }
+    }
+
+    auto execute() -> void {
+        const auto readByte = [this]() -> ByteCode::Instruction { 
+            return static_cast<ByteCode::Instruction>(*ip++); 
+        };
+
+        ByteCode::Instruction instruction;
+
+        while (true && ip != std::end(bytecode)) {
+            switch (instruction = readByte()) {
+            case ByteCode::Instruction::PRINT:
+                std::cout << std::visit(PrintVisitor{}, pop()) << '\n';
+                break;
+            case ByteCode::Instruction::ADD:
+                doBinaryOperation(BinaryOperators::ADD);
+                break;
+            case ByteCode::Instruction::SUB:
+                doBinaryOperation(BinaryOperators::SUB);
+                break;
+            case ByteCode::Instruction::PUSH_INT:
+                stack.push_back(readConstant<int>());
+                break;
+            case ByteCode::Instruction::PUSH_DOUBLE:
+                stack.push_back(readConstant<double>());
+                break;
+            }
+        }
+
+        //fmt::print("end stack\n");
+        //for (auto dump = stack; not dump.empty(); dump.pop_back()) {
+            //fmt::print("[ {} ]", std::visit(PrintVisitor{}, dump.back()));
+        //}
+        //fmt::print("\n");
+    }
+    
+private:
+
+    [[nodiscard]] auto pop() -> Value {
+        auto v = stack.back();
+        stack.pop_back();
+        return v;
+    }
+
+private:
+    std::span<ByteCode::Type> bytecode;
+    std::span<ByteCode::Type>::iterator ip;
+    std::vector<Value> stack;
+};
